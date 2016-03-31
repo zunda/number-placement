6d3e8c729a828f150d2ebe239dfabf5078ad7b22
zunda <zundan@gmail.com>
2016-03-30 20:01:54 -1000

    Fix indent

--- test/test_numbe-placement.rb
+++ test/test_numbe-placement.rb
@@ -2,34 +2,34 @@ require 'test/unit'
 require 'number-placement'
 
 class NumberPlacementNeighborsTest < Test::Unit::TestCase
-	include NumberPlacement
-	def setup
-		@n = Neighbors.new
-	end
+  include NumberPlacement
+  def setup
+    @n = Neighbors.new
+  end
 
-	def test_place
-		@n.occupy(0, 0)
-		assert_equal 1, @n.cells.size
-		assert_equal true, @n.floating[0]
-		@n.place(0, 0, 0)
-		assert_equal 0, @n.cells.size
-		assert_equal false, @n.floating[0]
-	end
+  def test_place
+    @n.occupy(0, 0)
+    assert_equal 1, @n.cells.size
+    assert_equal true, @n.floating[0]
+    @n.place(0, 0, 0)
+    assert_equal 0, @n.cells.size
+    assert_equal false, @n.floating[0]
+  end
 
-	def test_place_value_taken
-		@n.occupy(0, 0)
-		@n.occupy(0, 1)
-		@n.place(0, 0, 0)
-		assert_raise PlacementError do
-			@n.place(0, 1, 0)
-		end
-	end
+  def test_place_value_taken
+    @n.occupy(0, 0)
+    @n.occupy(0, 1)
+    @n.place(0, 0, 0)
+    assert_raise PlacementError do
+      @n.place(0, 1, 0)
+    end
+  end
 
-	def test_place_cell_taken
-		@n.occupy(0, 0)
-		@n.place(0, 0, 0)
-		assert_raise PlacementError do
-			@n.place(0, 0, 1)
-		end
-	end
+  def test_place_cell_taken
+    @n.occupy(0, 0)
+    @n.place(0, 0, 0)
+    assert_raise PlacementError do
+      @n.place(0, 0, 1)
+    end
+  end
 end
